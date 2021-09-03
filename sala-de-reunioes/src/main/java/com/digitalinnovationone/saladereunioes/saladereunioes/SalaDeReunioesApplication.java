233ab83728282ed7d21f29e5ba1659c026005af5
fabiopereirareis <espynho23@gmail.com>
2021-09-02 22:46:14 -0300

    my first commit

---
+++ sala-de-reunioes/src/main/java/com/digitalinnovationone/saladereunioes/saladereunioes/SalaDeReunioesApplication.java
@@ -0,0 +1,13 @@
+package com.digitalinnovationone.saladereunioes.saladereunioes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalaDeReunioesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalaDeReunioesApplication.class, args);
+	}
+
+}
---
+++ sala-de-reunioes/src/test/java/com/digitalinnovationone/saladereunioes/saladereunioes/SalaDeReunioesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalinnovationone.saladereunioes.saladereunioes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalaDeReunioesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
